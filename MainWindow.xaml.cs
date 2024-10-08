4fae83bd934f8ba40db912734d997477c4c1a29b
p10tr13 <160889621+p10tr13@users.noreply.github.com>
2024-10-08 19:40:42 +0200

    Podstawowe dodawanie nowych krawędzi

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,98 @@ namespace GK_Proj_1
         {
             InitializeComponent();
         }
+
+        private bool drawing = false;
+        private Figure drawingFigure = new Figure();
+        private Edge drawingAnimationEdge = null;
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pt = e.GetPosition(Canva);
+            if (drawing && drawingFigure.Edges.Count != 0 && (Math.Abs(pt.X - drawingAnimationEdge.p2.X) > 2 || Math.Abs(pt.Y - drawingAnimationEdge.p2.Y) > 2))
+            {
+                drawingAnimationEdge.p2 = e.GetPosition(Canva);
+                Redraw();
+            }
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point pt = e.GetPosition(this);
+
+            if (!drawing && drawingFigure.Edges.Count <= 1)
+            {
+                drawingAnimationEdge = new Edge(pt, pt);
+                drawing = true;
+                drawingFigure.Edges.Add(drawingAnimationEdge);
+                Redraw();
+                return;
+            }
+            if (drawing)
+            {
+                drawingFigure.Edges.Add(new Edge(drawingAnimationEdge.p1, pt));
+                drawingAnimationEdge.p1 = pt;
+                Redraw();
+                return;
+            }
+        }
+
+        private void Redraw()
+        {
+            Canva.Children.Clear();
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext dc = drawingVisual.RenderOpen())
+            {
+                drawingFigure.Draw(dc);
+            }
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)Canva.ActualWidth, (int)Canva.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(drawingVisual);
+            Image img = new Image { Source = rtb };
+            Canva.Children.Add(img);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+
+    public class Figure
+    {
+        public List<Edge> Edges { get; set; }
+        public bool IsSealed { get; set; }
+
+        public Figure()
+        {
+            Edges = new List<Edge>();
+        }
+
+        public void AddEdge(Edge edge)
+        {
+            Edges.Add(edge);
+        }
+
+        public void Draw(DrawingContext dc)
+        {
+            foreach (Edge edge in Edges)
+            {
+                edge.Draw(dc);
+            }
+        }
+    }
+
+    public class Edge
+    {
+        public Point p1;
+        public Point p2;
+
+        public Edge(Point pnt1, Point pnt2)
+        {
+            p1 = pnt1;
+            p2 = pnt2;
+        }
+
+        public void Draw(DrawingContext dc)
+        {
+            Pen pen = new Pen(Brushes.CadetBlue, 3);
+            dc.DrawLine(pen, p1, p2);
+        }
     }
 }
